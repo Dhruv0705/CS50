6ffaa88038fbb95dfad89438456ebf9fe1142836
Dhruv0705 <dhruvpatel329@yahoo.com>
2023-03-26 20:13:24 -0500

    Added CS50

---
+++ CS50W/Python/lecture2.py
@@ -0,0 +1,177 @@
+#       Formatting Strings
+
+name = str(input("Name: "))
+print(f"Hello, {name}")
+
+print("_____________________")
+
+#       Conditionsd
+
+n = int(input("Number: "))
+if n>0:
+    print(n, "is positive")
+elif n<0:
+    print(n, "is negitve")
+else:
+    print(n, " is equal to n")
+
+print("_____________________")
+
+#       Sequence
+
+name1= "Dhruv"
+print (name1[0])
+#List [sequence of mutable values]
+nameList = ["Dhruv", "Jay"]
+print (nameList[1])
+
+nameList.append("Fin")
+nameList.sort()
+print(nameList)
+
+#Tupple (seuence of immutable values)
+corridinateX=10.0
+corridanateY=20.0
+corridanate = (10.0,20.0)
+
+#Set
+s= set()
+s.add(1)
+s.add(2)
+s.add(3)
+s.add(3)
+s.add(4)
+print(s)
+
+s.remove(2)
+print(s)
+
+print(f"The set has {len(s)} elements.")
+
+print("_____________________")
+
+#       Loops
+
+for i in [0,1,2,3]:
+    print(i)
+print("_____________________")
+for i in range(4):
+    print(i)
+print("_____________________")
+for i in nameList:
+    print(i)
+print("_____________________")
+for i in name1:
+    print(i)
+print("_____________________")
+
+#       Dictionaries
+
+houses={"Harry": "Gryffindor", "Draco": "Slytherin"}
+houses["Hermione"] = "Gryffindor"
+print(houses["Harry"])
+print(houses["Hermione"])
+print("_____________________")
+
+#       Functions
+
+def square(x):
+    return x * x 
+
+for i in range(6):
+    print(f"The square of {i} is {square(i)} ")
+
+print("_____________________")
+#     Import Options
+#from (file name) import (function)
+#       OR
+# import (filename) 
+# call function (filename).(function) 
+
+#       Object-Orianted (Class)
+
+class Point():
+    def __init__(self, input1, input2):
+        self.x = input1
+        self.y = input2
+    
+p=Point(2,8)
+print(p.x)
+print(p.y)
+
+print("_____________________")
+
+# Adding person to flight  
+
+class Flight():
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.passangers= []
+
+    def add_passanger(self, passanger_name):
+        if not self.open_seat():
+            return False
+        self.passangers.append(passanger_name)
+        return True
+
+    def open_seat(self):
+        return self.capacity- len(self.passangers)
+
+flight=Flight(3)
+
+people= ["Dom", "Daniel", "Tom", "Fin"]
+for person in people:
+    if flight.add_passanger(person):
+        print(f"Added {person} to flight successfully ")
+    else:
+        print(f"No available seats for {person}")
+
+print("_____________________")
+#       Functional Programming
+# Decorators
+
+def announce(f):
+    def wrapper():
+        print("About to run the function....")
+        f()
+        print("Done with the function.")
+    return wrapper
+
+@announce
+def hello():
+    print("Hello World!!")
+
+hello()
+
+print("_____________________")
+
+# Lamda
+
+people1= [
+    {"name": "Harry", "house": "Gryffindor"},
+    {"name": "Cho", "house": "Ravenclaw"},
+    {"name": "Draco", "house": "Slytherin"}
+]
+
+people1.sort(key=lambda person: person["name"])
+
+print(people1)
+
+print("_____________________")
+
+import sys
+
+try:
+    x = int(input("x: "))
+    y = int(input("y: "))
+except ValueError:
+    print("Error: Invalid input.")
+    sys.exit(1)
+
+try: 
+    result = x/y
+except ZeroDivisionError:
+    print("Error: Cannot divide by 0.")
+    sys.exit(1)
+
+print(f"{x} / {y} = {result}")
---
+++ CS50x (Intro To CS)/Week1/Week1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+
+
+int main(void){
+    
+    printf("Hello, CS50! \n");
+    
+    char chr;
+    printf("Enter a character: ");
+    scanf("%c",&chr);     
+    printf("You entered %c.\n", chr);  
+
+    char name[50];
+    printf("What is your name?");
+    scanf("%c",&name);
+    printf("My name is %c.\n", name);
+
+    printf("Hello, CS50! \n");
+
+}
